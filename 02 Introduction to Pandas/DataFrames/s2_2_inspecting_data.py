442f2da2fbfdca7bfc8a5043759043a3d200ba4f
erikhuss92 <62145702+erikhuss92@users.noreply.github.com>
2020-03-31 09:59:15 +0200

    Lecture on inspecting Data using Pandas DataFrames

--- 02 Introduction to Pandas/DataFrames/s2_2_inspecting_data.py
+++ 02 Introduction to Pandas/DataFrames/s2_2_inspecting_data.py
@@ -1,36 +1,36 @@
 import pandas as pd
 
-df = pd.read_csv('../../sample_data/02 Introduction to Pandas/intel.csv')
+df = pd.read_csv('C:/Users/lisa.andersson/github/python_pandas/sample_data/02 Introduction to Pandas/intel.csv')
 
 # Print df to make sure it works
 # print(df)
 
 # How to check data type
-# print(type(df))
+#print(type(df))
 
 # How to check df shape
-# print(df.shape)
+#print(df.shape)
 
 # View column names
-# print(df.columns)
+#print(df.columns)
 
 # Inspect first tows of data
-# print(df.head(10))
+#print(df.head(2))
 
 # Inspect last rows of data
-# print(df.tail(2))
+#print(df.tail(10))
 
 # View summary df info
-# print(df.info())
+#print(df.info())
 
 # View open column
-# pen = df['Open']
-# print(open)
+#open = df['Open']
+#print(open)
 
 #print(open.head())
 
 # View one or more columns side by side
-# print(df[['Open', 'Close']].head())
+#print(df[['Open', 'Close']].head())
 
 # Using the describe method
-# print(df.describe())
+#print(df.describe())
